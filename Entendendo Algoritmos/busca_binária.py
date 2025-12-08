10cc87937b75b99cc4974a13686435bc721404b9
MarcelloAnthony20 <anthonymarcello20@gmail.com>
2025-12-08 09:54:21 -0300

    docs(entendendo-algortimos): adicionar testes de busca binaria

--- Entendendo Algoritmos/busca_binária.py
+++ Entendendo Algoritmos/busca_binária.py
@@ -1,13 +1,13 @@
-def busca_binaria(lista:list, alvo:int) -> int:
+def busca_binaria(lista:list, alvo:int) -> int | None:
     '''
     Realiza uma busca binária em uma lista ordenada para encontrar o índice do elemento alvo.
 
     Parâmetros:
-    lista (list): Uma lista ordenada de elementos.
-    alvo (int): O elemento que se deseja encontrar na lista.
+        lista (list): Uma lista ordenada de elementos.
+        alvo (int): O elemento que se deseja encontrar na lista.
 
     Retorna:
-    int: O índice do elemento alvo na lista, ou null se o elemento não for encontrado.
+        int | None: O índice do elemento alvo ou None se não for encontrado.
     '''
 
     esquerda = 0
@@ -24,4 +24,10 @@ def busca_binaria(lista:list, alvo:int) -> int:
 
     return None
 
-print(busca_binaria([1,2,3,4,5],6))
+def test_busca_binaria():
+    assert busca_binaria([1,2,3,4,5],5) == 4
+    assert busca_binaria([1,2,3,4,5],6) == None
+    assert busca_binaria(['a','b','c','d','e'],'c') == 2
+    print("✅ test_selection_sort: Todos os testes passaram!")
+
+test_busca_binaria()
